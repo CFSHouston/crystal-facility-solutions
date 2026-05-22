a26e9cb0a929c485d4c837b1f18a54b4ca80a8af
CFSHouston <admin@cfshouston.com>
2026-05-22 10:36:06 -0500

    Videos are renamed

--- js/modules/hero-video.js
+++ js/modules/hero-video.js
@@ -8,15 +8,15 @@
 
     const CONFIG = {
         videos: [
-            'videos/video.mp4',
-            'videos/video4.mp4',
-            'videos/video6.mp4',
-            'videos/video8.mp4',
-            'videos/video2.mp4',
-            'videos/video3.mp4',
-            'videos/video5.mp4',
-            'videos/video7.mp4',
-            'videos/video1.mp4'
+            'Videos/Video.mp4',
+            'Videos/Video4.mp4',
+            'Videos/Video6.mp4',
+            'Videos/Video8.mp4',
+            'Videos/Video2.mp4',
+            'Videos/Video3.mp4',
+            'Videos/Video5.mp4',
+            'Videos/Video7.mp4',
+            'Videos/Video1.mp4'
         ],
         playbackRate: 1.0,        // Slow motion (30% speed)
         crossfadeDuration: 1000,  // 1.2s fade between videos
